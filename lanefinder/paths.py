68f6d5c788bf81eabba4ecfc279f9930b91cd691
max-kazak <m@xkazak.com>
2020-05-11 11:31:10 -0600

    refactor data locations

---
+++ lanefinder/paths.py
@@ -0,0 +1,7 @@
+FILE_VIDEO_1 = 'data/videos/project_video.mp4'
+FILE_VIDEO_2 = 'data/videos/challenge_video.mp4'
+FILE_VIDEO_3 = 'data/videos/harder_challenge_video.mp4'
+
+DIR_CAMERA_CAL = 'data/camera_cal'
+DIR_TEST_IMG = 'data/test_images'
+DIR_OUTPUT_IMG = 'data/output_images'
